5cb262028807339db61b3a454b51e548415055fd
yelynnpaing <95997370+yelynnpaing@users.noreply.github.com>
2022-11-08 22:10:36 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//using class component
+/*
+import React from "react";
+class App extends React.Component {
+  state = {
+    users : [
+      {id : 1, first_name : "Bob"},
+      {id : 2, first_name : "helo"},
+    ]
+  }
+
+  componentDidMount () {
+    fetch('https://reqres.in/api/users')
+    .then( res => res.json()) 
+    .then( json => {
+      console.log(json.data)
+      this.setState ({users:json.data})
+    });
+    
+  } 
+  
+
+  render(){
+    return(
+      <div>
+        <ul>
+          {this.state.users.map( u =>
+            <li key={u.id}>
+              {u.first_name}
+            </li>
+            )}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+*/
+
+//using functional component
+import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const App = props => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
+    .then( res => res.json())
+    .then ( json =>{
+      console.log(json.data);
+      setUsers(json.data)
+    })
+  }, []);
+
+  const Add = () => {
+    fetch('https://reqres.in/api/users', {
+      method : "POST",
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify({first_name : "Tom"})
+    }).then (res => res.json()).then(tom => {
+      setUsers([...users, tom])
+    })
+  }
+
+  return (
+    <div className='App-header'>
+      <ul>
+        {users.map( u => 
+          <li key={u.id}>{u.first_name}</li>
+          )}
+      </ul>
+      <button onClick={Add}>Add User</button>
+    </div>
+  )
+}
+
+export default App
